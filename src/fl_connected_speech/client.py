3b8a527cabb02c4dfe06034f0e4917caa8fa00b3
helena-balabin <helena.balabin@kuleuven.be>
2024-01-07 21:15:56 +0100

    WIP: Add a first working version of the federated learning setup

--- src/fl_connected_speech/client.py
+++ src/fl_connected_speech/client.py
@@ -1,9 +1,11 @@
-import random
+import glob
+import os
 from collections import OrderedDict
 
+import evaluate
 import flwr as fl
 import torch
-from datasets import load_dataset, load_metric
+from datasets import load_dataset, concatenate_datasets, ClassLabel
 from torch.utils.data import DataLoader
 from transformers import (
     AdamW,
@@ -12,106 +14,149 @@ from transformers import (
     DataCollatorWithPadding,
 )
 
+# Parameters
+MODEL_BASE = "Unbabel/xlm-roberta-comet-small"  # TODO Replace with a more sensible model that is smaller than 512MB
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+BATCH_SIZE = 8
+EPOCHS = 5
+METRICS = ["accuracy", "precision", "recall", "f1"]
+LABELS = sorted(os.listdir("../../data/input"))
+label_column = ClassLabel(names=LABELS)
 
 
 def load_data():
-    """Load IMDB data (training and eval)"""
-    raw_datasets = load_dataset("imdb")
-    raw_datasets = raw_datasets.shuffle(seed=42)
+    """Load the data for each diagnostic group."""
+    raw_datasets = []
+    for label in LABELS:
+        # Check if the folder is empty
+        if len(glob.glob(f"../../data/input/{label}/*.txt")) == 0:
+            continue
+        # Load from text files in each label-specific sub-folder
+        label_specific_dataset = load_dataset(
+            "text",
+            data_dir=f"../../data/input/{label}",
+        )["train"]
+        # Add 'label' column to each split
+        label_specific_dataset = label_specific_dataset.add_column(
+            "labels", [label] * len(label_specific_dataset)
+        )
+        # Add to list of datasets
+        raw_datasets.append(label_specific_dataset)
 
-    # remove unnecessary data split
-    del raw_datasets["unsupervised"]
+    # Concatenate all datasets into one
+    raw_dataset = concatenate_datasets(raw_datasets)
+    # Shuffle the entries
+    raw_dataset = raw_dataset.shuffle(seed=42)
 
-    tokenizer = AutoTokenizer.from_pretrained("albert-base-v2")
+    # Add label column
+    raw_dataset = raw_dataset.cast_column("labels", ClassLabel(num_classes=len(LABELS), names=LABELS))
 
-    # random 10 samples
-    population = random.sample(range(len(raw_datasets["train"])), 10)
-
-    tokenized_datasets = raw_datasets.map(
-        lambda examples: tokenizer(examples["text"], truncation=True), batched=True
+    # Tokenize the dataset
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_BASE)
+    tokenized_datasets = raw_dataset.map(
+        lambda examples: tokenizer(examples["text"], truncation=True), batched=True,
     )
-    tokenized_datasets["train"] = tokenized_datasets["train"].select(population)
-    tokenized_datasets["test"] = tokenized_datasets["test"].select(population)
+    # Split into train and test (stratified)
+    tokenized_datasets = tokenized_datasets.train_test_split(
+        test_size=0.2, shuffle=True, seed=42, stratify_by_column="labels",
+    )
 
     tokenized_datasets = tokenized_datasets.remove_columns("text")
-    tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
 
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-    trainloader = DataLoader(
+    train_loader = DataLoader(
         tokenized_datasets["train"],
         shuffle=True,
-        batch_size=32,
+        batch_size=BATCH_SIZE,
         collate_fn=data_collator,
     )
 
-    testloader = DataLoader(
-        tokenized_datasets["test"], batch_size=32, collate_fn=data_collator
+    test_loader = DataLoader(
+        tokenized_datasets["test"], batch_size=BATCH_SIZE, collate_fn=data_collator,
     )
 
-    return trainloader, testloader
+    return train_loader, test_loader
 
 
-def train(net, trainloader, epochs):
-    optimizer = AdamW(net.parameters(), lr=5e-5)
-    net.train()
+def train(model, train_loader, epochs):
+    """Train the model for a given number of epochs."""
+    optimizer = AdamW(model.parameters(), lr=5e-5)
+    model.train()
+
     for _ in range(epochs):
-        for batch in trainloader:
+        for batch in train_loader:
             batch = {k: v.to(DEVICE) for k, v in batch.items()}
-            outputs = net(**batch)
+            outputs = model(**batch)
             loss = outputs.loss
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
 
 
-def test(net, testloader):
-    metric = load_metric("accuracy")
+def test(model, test_loader):
+    """Evaluate the trained model on the test set."""
+    # Initialize the evaluation variables
+    evaluations = []
+    all_metrics = {}
     loss = 0
-    net.eval()
-    for batch in testloader:
+    model.eval()
+
+    # Get scores for each batch
+    for batch in test_loader:
         batch = {k: v.to(DEVICE) for k, v in batch.items()}
         with torch.no_grad():
-            outputs = net(**batch)
+            outputs = model(**batch)
         logits = outputs.logits
         loss += outputs.loss.item()
         predictions = torch.argmax(logits, dim=-1)
-        metric.add_batch(predictions=predictions, references=batch["labels"])
-    loss /= len(testloader.dataset)
-    accuracy = metric.compute()["accuracy"]
-    return loss, accuracy
+        evaluations.append({"predictions": predictions, "references": batch["labels"]})
+
+    loss /= len(test_loader.dataset)
+    # Compute each metric
+    for metric in METRICS:
+        metric_func = evaluate.load(metric)
+        for ev in evaluations:
+            metric_func.add_batch(predictions=ev["predictions"], references=ev["references"])
+        if metric == "accuracy":
+            all_metrics[metric] = metric_func.compute()[metric]
+        else:
+            all_metrics[metric] = metric_func.compute(labels=range(len(LABELS)), average="micro")[metric]
+    print(all_metrics)
+    return loss, all_metrics
 
 
-net = AutoModelForSequenceClassification.from_pretrained(
-    "albert-base-v2", num_labels=2
-).to(DEVICE)
-
-trainloader, testloader = load_data()
+# Initialize model and data loaders
+cls_model = AutoModelForSequenceClassification.from_pretrained(MODEL_BASE, num_labels=len(LABELS)).to(DEVICE)
+tr_loader, te_loader = load_data()
 
 
-# Flower client
-class IMDBClient(fl.client.NumPyClient):
+class ClassificationClient(fl.client.NumPyClient):
+    """Flower client for the neurodegenerative disease classification task."""
     def get_parameters(self, config):
-        return [val.cpu().numpy() for _, val in net.state_dict().items()]
+        """Return the current parameters of the model, used by the server to obtain the global parameters."""
+        return [val.cpu().numpy() for _, val in cls_model.state_dict().items()]
 
-    def set_parameters(self, parameters):
-        params_dict = zip(net.state_dict().keys(), parameters)
+    def set_parameters(self, parameters):  # noqa
+        """Set the parameters of the model through the parameters obtained from the server."""
+        params_dict = zip(cls_model.state_dict().keys(), parameters)
         state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
-        net.load_state_dict(state_dict, strict=True)
+        cls_model.load_state_dict(state_dict, strict=True)
 
     def fit(self, parameters, config):
+        """Train the model on the training set."""
         self.set_parameters(parameters)
         print("Training Started...")
-        train(net, trainloader, epochs=1)
+        train(cls_model, tr_loader, epochs=EPOCHS)
         print("Training Finished.")
-        return self.get_parameters(config={}), len(trainloader), {}
+        return self.get_parameters(config={}), len(tr_loader), {}
 
     def evaluate(self, parameters, config):
+        """Evaluate the model on the test set."""
         self.set_parameters(parameters)
-        loss, accuracy = test(net, testloader)
-        return float(loss), len(testloader), {"accuracy": float(accuracy)}
+        loss, all_metrics = test(cls_model, te_loader)
+        return float(loss), len(te_loader), all_metrics
 
 
-# Start client
-fl.client.start_numpy_client(server_address="127.0.0.1:8080", client=IMDBClient())
-
+# Start client (training and evaluation)
+# TODO server address
+fl.client.start_numpy_client(server_address="127.0.0.1:8080", client=ClassificationClient())
--- src/fl_connected_speech/server.py
+++ src/fl_connected_speech/server.py
@@ -1,7 +1,36 @@
+from typing import Tuple, List
+
 import flwr as fl
+from flwr.common import Metrics
+
+# Parameters
+# TODO increase the number of rounds
+ROUNDS = 3
+
+
+# Add a function that aggregates all metrics
+def get_weighted_av_metrics(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    """Aggregate all the metrics from all clients.
+
+    :param metrics: List of tuples (num_examples, metrics) for each client
+    :type metrics: List[Tuple[int, Metrics]]
+    :return: A dictionary with the weighted average of each metric
+    :rtype: Metrics
+    """
+    all_metrics = list(metrics[0][1].keys())
+    final_metrics = {}
+    # Create a weighted average of each metric by number of examples used in each client
+    for metric in all_metrics:
+        final_metrics[metric] = sum([m[1][metric] * m[0] for m in metrics]) / sum([m[0] for m in metrics])
+
+    return final_metrics
+
 
 # Start server
 fl.server.start_server(
     server_address="0.0.0.0:8080",
-    config=fl.server.ServerConfig(num_rounds=3),
+    config=fl.server.ServerConfig(num_rounds=ROUNDS),
+    strategy=fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=get_weighted_av_metrics),
 )
+# TODO potentially add some mlflow logging
+# TODO save the model
