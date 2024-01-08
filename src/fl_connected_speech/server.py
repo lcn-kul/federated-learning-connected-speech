e34d169fa77bac857577c1bedf4b1fb79e12f67c
helena-balabin <helena.balabin@kuleuven.be>
2024-01-08 16:10:56 +0100

    Add basic mlflow logging and model saving

--- src/fl_connected_speech/server.py
+++ src/fl_connected_speech/server.py
@@ -1,10 +1,58 @@
-from typing import Tuple, List
+import os
+from typing import Tuple, List, Union, Dict, Optional
 
 import flwr as fl
-from flwr.common import Metrics
+import mlflow
+import numpy as np
+import torch
+from dotenv import load_dotenv
+from flwr.common import Metrics, FitRes, Scalar, Parameters
+from flwr.server.client_proxy import ClientProxy
+from transformers import AutoModelForSequenceClassification
 
 # Parameters
 ROUNDS = 5
+MODEL_BASE = "xlm-roberta-base"
+LABELS = sorted(os.listdir("../../data/input"))
+
+# Initialize mlflow
+load_dotenv()
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+mlflow.set_experiment(experiment_name=f"federated_learning_connected_speech")
+
+# Load a model (only used to get the parameter state dict)
+cls_model = AutoModelForSequenceClassification.from_pretrained(MODEL_BASE, num_labels=len(LABELS))
+model_output_dir = os.getenv("OUTPUT_DIR")
+
+
+class SaveModelStrategy(fl.server.strategy.FedAvg):
+    def aggregate_fit(
+        self,
+        server_round: int,
+        results: List[Tuple[fl.server.client_proxy.ClientProxy, fl.common.FitRes]],
+        failures: List[Union[Tuple[ClientProxy, FitRes], BaseException]],
+    ) -> Tuple[Optional[Parameters], Dict[str, Scalar]]:
+        """Aggregate model weights using weighted average and store checkpoint"""
+
+        # Call aggregate_fit from base class (FedAvg) to aggregate parameters and metrics
+        aggregated_parameters, aggregated_metrics = super().aggregate_fit(server_round, results, failures)
+
+        if aggregated_parameters is not None:
+            print(f"Saving round {server_round} aggregated_parameters...")
+
+            # Convert `Parameters` to `List[np.ndarray]`
+            aggregated_ndarrays: List[np.ndarray] = fl.common.parameters_to_ndarrays(aggregated_parameters)
+
+            # Convert `List[np.ndarray]` to PyTorch`state_dict`
+            # Get the current model parameters
+            params_dict = zip(cls_model.state_dict().keys(), aggregated_ndarrays)
+            state_dict = {k: torch.tensor(v) for k, v in params_dict}
+            cls_model.load_state_dict(state_dict, strict=True)
+
+            # Save the model with huggingface
+            cls_model.save_pretrained(os.path.join(model_output_dir, "picture_description", f"round_{server_round}"))
+
+        return aggregated_parameters, aggregated_metrics
 
 
 # Add a function that aggregates all metrics
@@ -22,15 +70,19 @@ def get_weighted_av_metrics(metrics: List[Tuple[int, Metrics]]) -> Metrics:
     for metric in all_metrics:
         final_metrics[metric] = sum([m[1][metric] * m[0] for m in metrics]) / sum([m[0] for m in metrics])
 
+    for metric, value in final_metrics.items():
+        mlflow.log_metric(metric, value)
+
     return final_metrics
 
 
 # Start server
-fl.server.start_server(
-    server_address="0.0.0.0:8080",
-    config=fl.server.ServerConfig(num_rounds=ROUNDS),
-    strategy=fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=get_weighted_av_metrics),
-    grpc_max_message_length=int(2e9),
-)
-# TODO potentially add some mlflow logging
-# TODO save the model
+with mlflow.start_run(run_name="picture_description"):
+    fl.server.start_server(
+        server_address="0.0.0.0:8080",
+        config=fl.server.ServerConfig(num_rounds=ROUNDS),
+        strategy=SaveModelStrategy(
+            evaluate_metrics_aggregation_fn=get_weighted_av_metrics,
+        ),
+        grpc_max_message_length=int(2e9),
+    )
